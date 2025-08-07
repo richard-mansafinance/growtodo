411835887e1f13ccad99f974defdfbcdf051e52a
otinomz <awesomeotinomz@gmail.com>
2025-08-07 15:41:09 +0100

    feat: documentation with swagger

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,21 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserDto } from './dto/user.dto';
-
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiOperation,
+} from '@nestjs/swagger';
+@ApiBearerAuth()
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Post('register')
+  @ApiOperation({ summary: 'Register a new user' })
+  @ApiCreatedResponse({ description: 'User created successfully' })
+  @ApiBadRequestResponse({ description: 'Invalid credentials' })
   async register(@Body() userDto: UserDto) {
     return await this.userService.register(userDto);
   }
